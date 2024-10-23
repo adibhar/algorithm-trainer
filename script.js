ba6a330f52060eb6b171f976231343c6a057feb2
Aditya Bhardwaj <adibhar20@gmail.com>
2024-10-22 21:09:21 -0500

    Basic PLL trainer page setup
    Need to fully add usable data to database before adding functionality

--- script.js
+++ script.js
@@ -6,13 +6,21 @@ let elapsedTime = 0;
 let timerInterval;
 let formatTime = (0.00).toFixed(2);
 
-//Oll data
+//OLL data
 let ollScrambles;
-let caseNames;
+let ollCaseNames;
 let currentOll;
 
-// Times array for Oll times
-let times = new Array(57).fill(null).map(() => []);
+//PLL data
+let pllScrambles;
+let pllCaseNames;
+let currentPll;
+
+// Times array for OLL times
+let ollTimes = new Array(57).fill(null).map(() => []);
+
+// Times array for PLL times
+let pllTimes = new Array(21).fill(null).map(() => []);
 
 // DOM elements
 const timerDisplay = document.getElementById('timer');
@@ -22,30 +30,32 @@ const timerResults = document.getElementById('times-box');
 
 //fetches OLL data from database
 async function fetchSpreadsheetData() {
-    const response = await fetch('https://docs.google.com/spreadsheets/d/1owibl9hlbkHKud2QcjV5LQEZvnQx3_oU9hC_ToF7lXY/export?format=csv');
-    const data = await response.text();
+    if (document.body.id === 'oll-page') {
+        const response = await fetch('https://docs.google.com/spreadsheets/d/1owibl9hlbkHKud2QcjV5LQEZvnQx3_oU9hC_ToF7lXY/export?format=csv');
+        const data = await response.text();
 
-    const rows = data.split('\n').slice(1, 58); 
+        const rows = data.split('\n').slice(1, 58); 
 
-    ollScrambles = rows.map(row => {
-        const cols = row.split(',');
-        const scramblesRead = cols[4].split(';').map(algorithm => algorithm.trim());
-        return scramblesRead
+        ollScrambles = rows.map(row => {
+            const cols = row.split(',');
+            const scramblesRead = cols[4].split(';').map(algorithm => algorithm.trim());
+            return scramblesRead
 
-    });
+        });
 
-    caseNames = rows.map(row => {
-        const cols = row.split(',');
-        const namesRead = cols[1]
-        return namesRead
+        ollCaseNames = rows.map(row => {
+            const cols = row.split(',');
+            const namesRead = cols[1]
+            return namesRead
 
-    });
-
+        });
+    }
 }
 
 
 //helper functions
 
+//starts timer and s
 function updateTimer() {
     elapsedTime = Date.now() - startTime;
     formatTime = (elapsedTime/1000).toFixed(2);
@@ -53,23 +63,27 @@ function updateTimer() {
 }
 
 function setRandomScramble() {
-    currentOll = Math.floor(Math.random() * (57) + 1); //refer to c1 below
-    let randomScr = Math.floor(Math.random() * (ollScrambles[currentOll - 1].length)); //finds a random scramble to set, in the index of the oll number
+    if (document.body.id === 'oll-page') {
+        currentOll = Math.floor(Math.random() * (57) + 1); //refer to c1 below
+        let randomScr = Math.floor(Math.random() * (ollScrambles[currentOll - 1].length)); //finds a random scramble to set, in the index of the oll number
 
-    //currentScramble.textContent = scrambles[currentOll - 1] + ' ' + currentOllPlusOne; //testing line
+        //currentScramble.textContent = scrambles[currentOll - 1] + ' ' + currentOllPlusOne; //testing line
 
-    currentScramble.textContent = ollScrambles[currentOll - 1][randomScr];
-    // c1. Since we want the OLL number to point to the exact item location (NOT index), we randomize currentOll from 1-57 
-    // and subtract 1 from it when indexing to have it correlate accordingly with times (which has indicies 0-56, since 57 oll exist)
+        currentScramble.textContent = ollScrambles[currentOll - 1][randomScr];
+        // c1. Since we want the OLL number to point to the exact item location (NOT index), we randomize currentOll from 1-57 
+        // and subtract 1 from it when indexing to have it correlate accordingly with times (which has indicies 0-56, since 57 oll exist)
 
-    // for example, we want the time correlating with OLL 1 to go in the first index of times, or times[0]
+        // for example, we want the time correlating with OLL 1 to go in the first index of times, or times[0]
+    }
 }
 
 function updateTimerResults() {
     let format = "";
-    for (let i = 0; i < times.length; i++) {
-        if (times[i].length !== 0) {
-            format += `<strong>${caseNames[i]}</strong>: ${times[i]}<br>`; 
+    if (document.body.id === 'oll-page') {
+        for (let i = 0; i < ollTimes.length; i++) {
+            if (ollTimes[i].length !== 0) {
+                format += `<strong>${ollCaseNames[i]}</strong>: ${ollTimes[i]}<br>`; 
+            }
         }
     }
     timerResults.innerHTML = format;
@@ -77,12 +91,12 @@ function updateTimerResults() {
 
 
 //start of program
-if (document.body.id === 'oll-page') {
+if (document.body.id === 'oll-page' || document.body.id === 'pll-page') {
     
     fetchSpreadsheetData().then(() => {
         setRandomScramble();
         // console.log("OLL ALGS" , ollScrambles);
-        // console.log("ALG NAMES", caseNames);
+        // console.log("ALG NAMES", ollCaseNames);
         updateTimerResults();
     });
     
@@ -100,10 +114,18 @@ document.addEventListener('keydown', function(event) {
     if (event.code === 'Space' && !event.repeat) {
         if (timerRunning) {
             timerRunning = false;
-            times[currentOll-1].push(formatTime); //refer to c1 above
-            setRandomScramble(ollScrambles); // do this after push statement so that the time is added to the correct index (setRandomScramble alters currentOLL)
-            updateTimerResults();
-            clearInterval(timerInterval);
+            if (document.body.id === 'oll-page') {
+                ollTimes[currentOll-1].push(formatTime); //refer to c1 above
+                setRandomScramble(); // do this after push statement so that the time is added to the correct index (setRandomScramble alters currentOLL)
+                updateTimerResults();
+                clearInterval(timerInterval);
+            }
+            if (document.body.id === 'pll-page') {
+                //pllTimes[currentPll-1].push(formatTime);
+                setRandomScramble();
+                updateTimerResults();
+                clearInterval(timerInterval);
+            }
         } else {
             event.preventDefault();
             setTimerColor('timer-red');
